b82732730b84b19de23aaffe83cf37d2bef9a9ed
Steven <steven@MSI-Steven>
2019-12-27 13:31:08 +0800

    加入專案檔案。

---
+++ Xamarin.Lab.PrismDialogService/Xamarin.Lab.PrismDialogService/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Xamarin.Lab.PrismDialogService.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ICommand OnShowAlert { get; set; }
+
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            this.OnShowAlert = new DelegateCommand(() =>
+              {
+                  pageDialogService.DisplayAlertAsync("我是Title", "我是Message", "確定");
+              });
+        }
+    }
+}
